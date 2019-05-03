dcac334d309f337b9b887d206853cd9499693066
Sahil Arya <sahilarya@systrome.net>
2019-05-04 00:33:40 +0530

    changes

---
+++ index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+
+restService.post("/echoV2", function(req, res) {
+  var speech = "";
+  if(req.body.queryResult && req.body.queryResult.parameters)
+  {
+    if(req.body.queryResult.parameters.mail)
+      speech = req.body.queryResult.parameters.mail;
+    if(req.body.queryResult.parameters.message)
+      speech +=req.body.queryResult.parameters.message; 
+    
+  }else{
+      speech = "Seems like some problem. Speak again.";
+  }
+      
+  return res.json({
+    fulfillmentText: speech,
+    fulfillmentMessages: [
+      {
+        "text": {
+          "text": [
+            speech
+          ]
+        }
+      }
+    ],
+    source: "webhook-echo-sample"
+  });
+});
+
+
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
